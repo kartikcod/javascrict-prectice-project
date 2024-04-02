db0b5c685a4c2d8143257ce2ffe7abbeb83f06d9
kartik mahato <118644047+kartikcod@users.noreply.github.com>
2024-04-02 21:38:06 +0530

    Add files via upload

---
+++ LargestNumber.js
@@ -0,0 +1,14 @@
+// function largestNumber(array) {
+//     return Math.max(...array)
+// }
+// const array = [45, 656, 56, 1, 11, 6]
+// const result = largestNumber(array)
+// console.log('The array of element of largest number:' + result)
+
+
+function smallNumber(array){
+    return Math.min(...array)
+}
+const array=[12,54,4,65,4,4,1]
+const result=smallNumber(array)
+console.log('the element of the smallNumber:'+result)
---
+++ Sum.js
@@ -0,0 +1,27 @@
+// console.log(" two number of sum");
+// var a = 5;
+// var b = 5;
+// sum = a + b;
+// console.log(sum)
+
+// const first_name = "kartik";
+// const second_name = "mahato";
+// const full_name = first_name + " "+second_name
+// console.log(full_name)
+
+// const a=10;
+// const b=12;
+// c=a+b;
+// console.log(c)
+
+//User input in two number
+
+// Get the two numbers from the user
+const num1 = parseInt(prompt('Enter the first number: '));
+const num2 = parseInt(prompt('Enter the second number: '));
+
+// Calculate the sum of the two numbers
+const sum = num1 + num2;
+
+// Print the sum to the console
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
